d3e15822767a86773c13d9e733261a6af30e3bff
TheSwaGiraffe <tojaxon@gmail.com>
2025-03-25 14:10:42 -0700

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "<h1>Welcome to My Python Website!</h1>"
+    return "<h1>Welcome to My Python Website!</h1> <hr>"
 
 if __name__ == "__main__":
     app.run(debug=True)
